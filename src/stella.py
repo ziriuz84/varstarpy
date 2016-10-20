507bf6e1067c50b76549b236b47f10de9477d1a2
Sirio Negri <ziriuz84@gmail.com>
2016-10-20 19:09:20 +0200

    Aggiunta la classe Stella con attributi

---
+++ src/stella.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+
+class Stella:
+    def __init__(self):
+        self.ID = 0
+        self.Nome = ""
+        self.Designazione = ""
+        self.LumMin = 0.0
+        self.LumMax = 0.0
