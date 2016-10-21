3a26408750c965635fd943b8189bd5d9c3987c4b
Sirio Negri <ziriuz84@gmail.com>
2016-10-21 12:55:34 +0200

    Aggiunta la funzione di aggiunta al database delle stelle

--- src/stella.py
+++ src/stella.py
@@ -8,3 +8,16 @@ class Stella:
         self.Designazione = ""
         self.LumMin = 0.0
         self.LumMax = 0.0
+
+    def Aggiungi(self, db):
+        cur = db.cursor()
+        sqlstatement = "SELECT COUNT(*) FROM STELLE"
+        cur.execute(sqlstatement)
+        self.ID = cur.fetchone()[0]
+        sqlstatement = "INSERT INTO STELLE VALUES(?, ?, ?, ?, ?);"
+        cur.execute(sqlstatement, (self.ID,
+                                   self.Nome,
+                                   self.Designazione,
+                                   self.LumMin,
+                                   self.LumMax))
+        db.commit()
