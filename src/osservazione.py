f8371378c56035544f750d1e3c295797a6d1a356
Sirio Negri <ziriuz84@gmail.com>
2016-10-21 12:43:34 +0200

    Aggiunta classe Osservazioni con relativi attibuti

---
+++ src/osservazione.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+
+class Osservazione:
+    def __init__(self):
+        self.ID = 0
+        self.Campagna = 0
+        self.Stella = 0
+        self.Luogo = ""
+        self.Strumento = ""
+        self.Seeing = 0
+        self.Data = ""
+        self.Misura = 0.0
+        self.StellaRif1 = 0.0
+        self.StellaRif2 = 0.0
+        self.Metodo = ""
+        self.Note = ""
